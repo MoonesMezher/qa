7c7ce89ea25a65da1efd99318fab7ce440d088ac
MoonesMezher <moonesmezher050@gmail.com>
2024-08-04 12:52:23 +0300

    ll

--- src/socket/online.js
+++ src/socket/online.js
@@ -143,7 +143,7 @@ const startMethod = async (item, socket, io) => {
                     console.log('deleted now');
                 }
                 
-                const threeMinAndHalf = 20 * 1000;
+                const threeMinAndHalf = 30 * 1000;
                 
                 setTimeout(finishGame,threeMinAndHalf); 
                 setTimeout(deleteRoom, threeMinAndHalf + 5000); 
